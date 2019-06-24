fb83ca3f59aac8fca61308f80040a9596b61aea8
Pavelrst <pavelr@campus.technion.ac.il>
2019-06-25 02:40:00 +0300

    similarity to ido added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from data_provider import dataProvider
 from cross_validation import crossValidator
 import numpy as np
+import pandas as pd
 
 from model import Keras_MLP
 from model import MLP_ensemble
@@ -13,22 +14,33 @@ def main():
     x_train, y_train = dp.get_train_xy(onehot_y=True)
     x_val, y_val = dp.get_val_xy(onehot_y=True)
     x_test = dp.get_test_xy()
+    id_test = dp.get_test_id()
 
-    #model = Keras_MLP(drop_p=0.1)
-    #model.fit(x_train,y_train,graphic=True)
+    # Cross validation Random search
+    # cv = crossValidator(train_x=x_train, train_y=y_train, num_of_folds=4, max_epochs=500)
+    # cv.rand_tune(iter=1000)
 
-    # Cross validation
-    #cv = crossValidator(train_x=x_train, train_y=y_train,
-    #                    num_of_folds=4, max_epochs=500)
-    #cv.tune_dropout()
-    #cv.tune_leaky_slope()
-    #cv.tune_hidden_layers()
-    #cv.rand_tune(iter=1000)
-
-    ensamble = MLP_ensemble('saved_models', party_dict)
-    ensamble.score(x_val, y_val)
+    # ensamble = MLP_ensemble('saved_models', party_dict)
+    #ensamble.score(x_val, y_val)
+    #ensamble.predict_winner(x_test)
     #ensamble.predict_vote_division(x_test)
+    #ensamble.write_pred_to_csv(x_test, id_test)
 
+    # Sanity check
+    similarity_to_ido()
+
+def similarity_to_ido():
+    ido = pd.read_csv('ido_predictions.csv')
+    pavel = pd.read_csv('test_predictions.csv')
+    ido_party = ido.pop('Vote').values
+    pavel_party = pavel.pop('Vote').values
+
+    acc = 0
+    for i,p in zip(ido_party, pavel_party):
+        if i == p:
+            acc += 1
+    acc = acc/100
+    print("Similarity to ido: ", acc, "%")
 
 if __name__ == "__main__":
     main()
